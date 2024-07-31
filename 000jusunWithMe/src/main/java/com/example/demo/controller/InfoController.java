afdef82393ac5945a1cd185d509efca53b26691f
jusunKim <jusun627@naver.com>
2024-07-31 22:44:01 +0900

    header - 헤더의 그룹명 클릭시 groupInfo와 연결 [주선]

--- 000jusunWithMe/src/main/java/com/example/demo/controller/InfoController.java
+++ 000jusunWithMe/src/main/java/com/example/demo/controller/InfoController.java
@@ -1,7 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.entity.GroupTable;
+import com.example.demo.entity.Users;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class InfoController {
@@ -11,9 +20,12 @@ public class InfoController {
 		
 	};
 	
-	@GetMapping("/groupInfo/groupInfo")
-    public void groupInfoView() {
-		
+	@GetMapping("/groupInfo/groupInfo/{groupNo}")
+    public String groupInfoView(@PathVariable long groupNo,HttpSession session) {
+//		GroupTable selectedGroup = (GroupTable)session.getAttribute("selectedGroup");
+//		List<Users> users = new ArrayList<>();
+//		//groupMember에서 해당 그룹에 들어가있는 userNo를 찾아서 리스트로 올
+		return "/groupInfo/groupInfo";
 	};
 	
 }
