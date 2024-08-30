2acfc59bc4de3a5211a52104f4676720a585edbc
ysooy <sksmsgovlgo@naver.com>
2024-08-30 15:50:18 +0900

    웹소켓 테스트를 위해 주석처리[수영]

--- 000jusunWithMe/src/main/java/com/example/demo/config/WebSocketHandlerConfig.java
+++ 000jusunWithMe/src/main/java/com/example/demo/config/WebSocketHandlerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.example.demo.handler.WebSocketHandler;
+//import com.example.demo.handler.WebSocketHandler;
 
 @Configuration
 @EnableWebSocket
@@ -14,13 +14,13 @@ public class WebSocketHandlerConfig implements WebSocketConfigurer {
 	
 	// 웹소켓 핸들러 등록하고 특정 경로에서 웹소켓 연결 허용
 
-    @Autowired 
-    private WebSocketHandler webSocketHandler;
+//    @Autowired 
+//    private WebSocketHandler webSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+//        registry.addHandler(webSocketHandler, "/ws")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();
     }
 }
